caddf47a136a6d3c2896739092fa6b4089243f4e
Stephane Leclercq <stephane.leclercq@gmail.com>
2013-04-13 17:31:20 +0200

    Introduce ticker example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,24 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.lang.ArrayUtils;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.FluentIterable;
-
-import com.sun.nio.zipfs.ZipPath;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import to.sparks.mtgox.MtGoxHTTPClient;
+import to.sparks.mtgox.model.AccountInfo;
+import to.sparks.mtgox.model.Lag;
+import to.sparks.mtgox.model.Order;
+import to.sparks.mtgox.model.Ticker;
 
 /**
  *
  */
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     private static final int A_LONG_TIME = 1000 * 60 * 60;
 
@@ -38,14 +32,48 @@ public class Main {
             ex.printStackTrace();
         }
 
-        while(true) {
-            System.out.println("Nothing here yet");
+//        while(true) {
             try {
-                Thread.sleep(A_LONG_TIME);
-            } catch (InterruptedException e) {
+                tickerExample();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+//            try {
+//                Thread.sleep(A_LONG_TIME);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        System.exit(0);
+    }
+
+    public static void tickerExample() throws Exception {
+
+        // Obtain a $USD instance of the API
+        ApplicationContext context = new ClassPathXmlApplicationContext("to/sparks/mtgox/example/Beans.xml");
+        MtGoxHTTPClient mtgoxUSD = (MtGoxHTTPClient) context.getBean("mtgoxUSD");
+
+        Lag lag = mtgoxUSD.getLag();
+        logger.info("Current lag: {}", lag.getLag());
+
+
+        Ticker ticker = mtgoxUSD.getTicker();
+        logger.info("Last price: {}", ticker.getLast().toPlainString());
+
+        // Get the private account info
+        AccountInfo info = mtgoxUSD.getAccountInfo();
+        logger.info("Logged into account: {}", info.getLogin());
+
+        Order[] openOrders = mtgoxUSD.getOpenOrders();
+
+        if (ArrayUtils.isNotEmpty(openOrders)) {
+            for (Order order : openOrders) {
+                logger.info("Open order: {} status: {} price: {}{} amount: {}", new Object[]{order.getOid(), order.getStatus(), order.getCurrency().getCurrencyCode(), order.getPrice().getDisplay(), order.getAmount().getDisplay()});
+            }
+        } else {
+            logger.info("There are no currently open bid or ask orders.");
         }
+
     }
 
 }
