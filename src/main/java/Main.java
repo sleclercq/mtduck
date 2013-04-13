6e7d70f5ffbb2036e1cf91ba54c09115265be360
Stephane Leclercq <stephane.leclercq@gmail.com>
2013-04-13 18:16:34 +0200

    Add ticker irc bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.commons.lang.ArrayUtils;
 
+import com.lair.mtduck.irc.TickerIRCMessenger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import to.sparks.mtgox.MtGoxHTTPClient;
@@ -20,8 +21,6 @@ public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    private static final int A_LONG_TIME = 1000 * 60 * 60;
-
     public static void main(String[] args) {
         Properties prop = new Properties();
 
@@ -32,20 +31,39 @@ public class Main {
             ex.printStackTrace();
         }
 
-//        while(true) {
-            try {
-                tickerExample();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-//            try {
-//                Thread.sleep(A_LONG_TIME);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+//        try {
+//            tickerExample();
+//        } catch (Exception e) {
+//            e.printStackTrace();
 //        }
+
+        try {
+            TickerIRCMessenger tickerIRCMessenger = new TickerIRCMessenger();
+            tickerIRCMessenger.setVerbose(true);
+            tickerIRCMessenger.connect(prop.getProperty("irc.server.host"), 6667, prop.getProperty("irc.server.password"));
+            tickerIRCMessenger.joinChannel(prop.getProperty("irc.channel"));
+            tickerIRCMessenger.sendMessage(prop.getProperty("irc.channel"), "...aaaaaand we're back!");
+            tickerIRCMessenger.sendMessage(prop.getProperty("irc.channel"), getTicker().getLast().toPlainString());
+        } catch (Exception e) { // TODO get rid of Exception in mtgox library
+            e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep(1000 * 10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.exit(0);
     }
+
+    public static Ticker getTicker() throws Exception {
+        // Obtain a $USD instance of the API
+        ApplicationContext context = new ClassPathXmlApplicationContext("to/sparks/mtgox/example/Beans.xml");
+        MtGoxHTTPClient mtgoxUSD = (MtGoxHTTPClient) context.getBean("mtgoxUSD");
+        return mtgoxUSD.getTicker();
+    }
+
 
     public static void tickerExample() throws Exception {
 
---
+++ src/main/java/com/lair/mtduck/irc/TickerIRCMessenger.java
@@ -0,0 +1,12 @@
+package com.lair.mtduck.irc;
+
+import org.jibble.pircbot.PircBot;
+
+/**
+ *
+ */
+public class TickerIRCMessenger extends PircBot {
+
+
+
+}
